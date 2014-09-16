1d1595cd7362175eb84e3e2e01d53f864967199d
Austin Cheney <austin.cheney@razorfish.com>
2014-09-15 22:21:38 -0500

    bug fixes

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Convert positive integers to human readable file sizes
 Use with Node
 -------------
 
-    var prettybyte = require("prettybyte");
-    prettybyte.size(123456789); // returns "117.73MB"
+    var prettybytes = require("prettybytes");
+    prettybytes.size(123456789); // returns "117.73MB"
 
 Use with Vanilla JS
 -------------------
 
-    prettybyte(1020); // returns "0.99KB"
+    prettybytes(1020); // returns "0.99KB"
--- prettybytes.js
+++ prettybytes.js
@@ -1,16 +1,22 @@
-var prettybyte = function (an_integer) {
+var prettybytes = function (an_integer) {
         "use strict";
 
         //find the string length of input and divide into triplets
-        var len   = Math.floor((an_integer.toString().length - 1) / 3) - 1,
+        var length  = an_integer.toString().length,
+            triples = (function () {
+                if (length < 22) {
+                    return Math.floor((length - 1) / 3);
+                }
+                return 8;
+            }()),
             //each triplet is worth an exponent of 1024 (2 ^ 10)
-            power = (function () {
-                var a = len,
+            power   = (function () {
+                var a = triples - 1,
                     b = 1024;
-                if (len === 0) {
+                if (triples === 0) {
                     return 0;
                 }
-                if (len === 1) {
+                if (triples === 1) {
                     return 1024;
                 }
                 do {
@@ -20,25 +26,25 @@ var prettybyte = function (an_integer) {
                 return b;
             }()),
             //kilobytes, megabytes, and so forth...
-            unit  = [
-                'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'
-            ]
-            output = "";
+            unit    = [
+                "", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"
+            ],
+            output  = "";
 
         if (typeof an_integer !== "number" || isNaN(an_integer) === true || an_integer < 0 || an_integer % 1 > 0) {
             //input not a positive integer
             output = "0.00B";
-        } else if (len === 0) {
+        } else if (triples === 0) {
             //input less than 1000
             output = an_integer + "B";
         } else {
             //for input greater than 999
-            ouptut = (Math.floor((an_integer / power) * 100) / 100).toFixed(2) + unit[len];
+            output = (Math.floor((an_integer / power) * 100) / 100).toFixed(2) + unit[triples];
         }
         //for Node.js compatibility return a method
         if (typeof exports === "object" || typeof exports === "function") {
             exports.size = function () {
-                return output
+                return output;
             };
         } else {
             //for vanilla JS just return the answer directly
